b242ecf0849dea162418a2e8cbf0ee38c7d23deb
Thiago Heraclides <thiagoheraclides@gmail.com>
2026-03-18 09:32:45 -0300

    Add project files.

---
+++ Controllers/PontoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Br.Com.ControleHoras.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PontoController(ILogger<PontoController> logger) : ControllerBase
+    {
+        private readonly ILogger<PontoController> _logger = logger;
+
+        [HttpPost(Name = "CadastrarNovoPeriodo")]
+        public IActionResult CadastrarPeriodo([FromBody] ushort ano, ushort mes)
+        {
+            return Ok();
+        }
+    }
+}
